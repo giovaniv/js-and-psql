2547186ed3bb676cca8c5197c574f8eee99c6051
giovaniv <giovaniv@gmail.com>
2018-06-26 23:47:47 +0000

    added knex migration and rollback

--- migrations/20180626234658_add.js
+++ migrations/20180626234658_add.js
@@ -2,8 +2,10 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('milestones', function(table){
+      table.increments('id').unsigned().primary();
       table.string('description').notNull();
       table.dateTime('date_achieved').notNull();
+      table.integer('famous_person_id').unsigned().references('id').inTable('famous_people');
       table.timestamps();
     })
   ])
